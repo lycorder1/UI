c07341575e7796137d774491ec8739740648664c
lycorder1 <834043365@qq.com>
2021-06-09 15:51:31 +0800

    test1

---
+++ test1/src/pages/shop/shop.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h1>商店</h1>
+        <a href="shopDetail.html">jump to shopDetail.html</a>
+         <a href="shopDetail.html">common</a>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "shop"
+    }
+</script>
+
+<style scoped>
+
+</style>
